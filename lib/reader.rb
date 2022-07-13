21a3bb8c631a016e18926bb3d709d45ea0bd7f98
mimipeshy <perisndanu@gmail.com>
2022-07-13 22:51:32 +0300

    add reader methods

--- lib/reader.rb
+++ lib/reader.rb
@@ -9,6 +9,27 @@ class RSSReader
         @url= url
     end
 
+    def display_rss
+        feed= parsed_feed(rss_feed)
+        save_feed(feed)
+    end
+
+    private
+
+    def rss_read
+        URI.open @url
+    rescue SocketError
+        'An issue occured opening the feed'
+    end
+
+    def parsed_feed(rss_feed)
+        RSS::Parser.parse(rss_feed)
+    end
+
+    def save_feed(rss_feed)
+    end
+    
+
    
 
 end
